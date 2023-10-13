eed8f0173010d7eb2a53f44d396336d90605f115
JackyIshmael <382948658@qq.com>
2023-10-13 17:32:46 +0800

    finish par1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,65 @@
-import { useState } from 'react'
-import './style/first.css'
+import React, { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
+const Button = (props) => {
+  const buttonName = props.buttonName
+  const buttonAction = props.buttonAction
+  return (
+    <button value={'asd'} onClick={buttonAction}>
+      {buttonName}
+    </button>
+  )
+}
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+
+  const getAll = () => {
+    return good + neutral + bad
+  }
+
+  const getScore = () => {
+    return good - bad
+  }
+
+  const getAverage = () => {
+    const res = getScore() / getAll()
+    return isNaN(res) ? 0 : res
+  }
+
+  const getPositive = () => {
+    const res = good / getAll()
+    return (isNaN(res) ? 0 : res) * 100
+  }
+  return (
+    <React.Fragment>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {getAll()}</div>
+      <div>average {getAverage()}</div>
+      <div>positive {getPositive()} %</div>
+    </React.Fragment>
+  )
+}
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  // 自定义2个函数作操作
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const shouldShowFeedback = () => {
+    return good + neutral + bad > 0
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
-      <div class="wave"></div>
+      <h1>give feedback</h1>
+      <Button buttonName={'good'} buttonAction={() => setGood(good + 1)} />
+      <Button buttonName={'neutral'} buttonAction={() => setNeutral(neutral + 1)} />
+      <Button buttonName={'bad'} buttonAction={() => setBad(bad + 1)} />
+      <h1>statistics</h1>
+      {shouldShowFeedback() ? <Statistics good={good} neutral={neutral} bad={bad} /> : <div>No feedback given</div>}
     </div>
   )
 }
